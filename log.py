c979e6d662032c5c4b729807e9beb2733d6667c9
unknown <jesse.greenfield@gmail.com>
2019-01-12 17:21:48 -0800

    fixed queries and connect except

--- log.py
+++ log.py
@@ -6,7 +6,7 @@ top_three_articles_report = {
     "query": """select articles.title, count(*) as views
                 from articles,
                 log
-                where log.path like '%'||articles.slug||'%'
+                where log.path = concat('/article/', articles.slug)
                 group by articles.title
                 order by views desc limit 3"""
 }
@@ -16,7 +16,7 @@ top_authors_by_views_report = {
     "query": """select authors.name, count(*) as views
                 from articles
                 inner join authors on articles.author = authors.id
-                inner join log on log.path like '%'||articles.slug||'%'
+                inner join log on log.path = concat('/article/', articles.slug)
                 group by authors.name
                 order by views desc"""
 }
@@ -24,7 +24,7 @@ top_authors_by_views_report = {
 days_with_1_perc_err_report = {
     "title": "Days with more than 1% of requests with errors",
     "query": """Select dayErrors.totalsDay,
-                ROUND(dayErrors.errorLogCount::NUMERIC/dayErrors.logCount, 2)
+                ROUND(dayErrors.errorLogCount::NUMERIC/dayErrors.logCount*100.0, 2)
                 ||'%' AS rate
                 FROM
                 ((
@@ -62,9 +62,8 @@ def print_report(report):
     query_results = get_query_results(report["query"])
     print(report["title"])
     for index, results in enumerate(query_results[0]):
-        print(
-            "\t" + str(index+1) + ". " + str(results[0]) +
-            " - " + str(results[1]) + " " + query_results[1][1][0])
+        print("{}. {} - {} {}"
+              .format(index+1, results[0], results[1], query_results[1][1][0]))
     return
 
 
@@ -74,7 +73,7 @@ def connect(db_name="news"):
         db = psycopg2.connect("dbname={}".format(db_name))
         cursor = db.cursor()
         return db, cursor
-    except:
+    except psycopg2.DatabaseError:
         print("Unable to connect to the database")
 
 
